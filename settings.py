ba2cbfb0158103d174be49e44fda20691f711605
rzarviv <rzarviv@gmail.com>
2018-09-04 19:13:41 +0300

    deleted unnecessary rooms

--- settings.py
+++ settings.py
@@ -61,15 +61,15 @@ REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 
 ROOMS = [
-    {
-        'name': 'econ101',
-        'display_name': 'Econ 101 class',
-        'participant_label_file': '_rooms/econ101.txt',
-    },
-    {
-        'name': 'live_demo',
-        'display_name': 'Room for live demo (no participant labels)',
-    },
+    # {
+    #     'name': 'econ101',
+    #     'display_name': 'Econ 101 class',
+    #     'participant_label_file': '_rooms/econ101.txt',
+    # },
+    # {
+    #     'name': 'live_demo',
+    #     'display_name': 'Room for live demo (no participant labels)',
+    # },
     {
         'name': 'ultimatum',
         'display_name': 'Room for ultimatum game',
