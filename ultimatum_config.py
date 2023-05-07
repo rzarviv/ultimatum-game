70a8bb2fcb61dddaa6eeccdcac71d0a2ef4ebf13
Roy Zerbib <rzarviv@gmail.com>
2023-05-07 22:39:11 +0300

    Update ultimatum_config.py

--- ultimatum_config.py
+++ ultimatum_config.py
@@ -4,7 +4,7 @@ CONFIG = {
     "db_address": "localhost",
     "db_name": "ultimatum",
     "db_username": "postgres",
-    "db_password": "royzerbib10",
+    "db_password": "*************",
     "db_schema_name": "users_data",
     "messages_thresholds": ["we indicate that you will probably get a very selfish offer",
                             "we indicate that you will probably get a very generous offer",
